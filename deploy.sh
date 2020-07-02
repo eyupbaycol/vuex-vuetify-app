1f6f1a6f66b60e64629dd0e72328133f9a520b82
Eyüp Bayçöl <eyup.bycl@gmail.com>
2020-07-02 09:21:47 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,15 @@ set -e
 
 npm run build
 
-cd dist;
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:eyupbaycol/vuex-vuetify-app.git master:gh-pages
+git push  origin master
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
